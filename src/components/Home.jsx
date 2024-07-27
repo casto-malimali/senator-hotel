75b6ca180e28ea7cca1b569796c191c6b27efe41
Casto MALIMALI <castomalimali@gmail.com>
2024-07-27 17:37:20 +0300

    all

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './layouts/Footer'
+import Spinner from './layouts/Spinner'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Spinner />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
